b8f7724bc2b8b5a8c152421b2a6828e2aa2b39b2
Ahua <git@ahua.space>
2025-01-24 16:12:31 +0800

    修复cloudflare 部署问题

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,14 @@
+// next.config.ts
 import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
-  trailingSlash: true,
-  webpack: (config, { isServer }) => {
-    config.cache = false;
-    if (!isServer) {
-      config.optimization = {
-        ...config.optimization,
-        minimize: true,
-        splitChunks: {
-          chunks: 'all',
-          maxSize: 200000,
-          minSize: 10000
-        }
-      }
-    }
-    return config;
-  }
+ output: 'static',
+ trailingSlash: true,
+ images: {
+   unoptimized: true
+ },
+ assetPrefix: '.',
+ basePath: ''
 }
 
 export default nextConfig
