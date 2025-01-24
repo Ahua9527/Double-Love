9bed2e564b3ed53a45af30ed9b9830dc71206873
Ahua <git@ahua.space>
2025-01-24 16:16:03 +0800

    修复next.config.ts 的 output 值错误和 assetPrefix 格式错误:

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
-// next.config.ts
 import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
- output: 'static',
- trailingSlash: true,
- images: {
-   unoptimized: true
- },
- assetPrefix: '.',
- basePath: ''
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: '/',
+  basePath: ''
 }
 
 export default nextConfig
